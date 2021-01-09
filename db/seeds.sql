cffa6ae2549463263f6d538f2c31bba2c04f801c
Cayman Heng <hengcayman@gmail.com>
2021-01-09 17:17:34 -0500

    created seeds.sql and inserted queries to populate 'burgers' table

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Portobello Mushroom Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beyond Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', false);
